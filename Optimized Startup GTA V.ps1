3073d6e62baaf86d6538395bffb185d3cb7b5bd9
Agent_E04 <58344071+jmsch23280866@users.noreply.github.com>
2024-04-11 20:29:26 +0800

    Update Optimized Startup GTA V.ps1

--- Optimized Startup GTA V.ps1
+++ Optimized Startup GTA V.ps1
@@ -7,7 +7,7 @@ Start-Sleep -Seconds 120
 # 檢查 GTA5 是否已經啟動
 $gtaProcess = Get-Process -Name "GTA5" -ErrorAction SilentlyContinue
 if ($gtaProcess) {
-    # 如果 GTA5 已經啟動，則終止 EpicGamesLauncher
+    # 如果 GTA5 已經啟動，則終止 EpicGamesLauncher.exe
     $epicProcess = Get-Process -Name "EpicGamesLauncher" -ErrorAction SilentlyContinue
     if ($epicProcess) {
         Stop-Process -Name "EpicGamesLauncher" -Force
